f1077a44efbadcae60f33dc18ec3297cd789a384
jaferabdella674-eng <jaferabdella674@gmail.com>
2026-02-21 21:45:53 +0300

    update website

---
+++ app.ts
@@ -0,0 +1,51 @@
+app.js
+import React, { useState } from "react";
+import "./App.css";
+
+function App() {
+
+  const [task, setTask] = useState("");
+  const [tasks, setTasks] = useState([]);
+
+  // Add Task
+  const addTask = () => {
+    if (task.trim() === "") return;
+
+    setTasks([...tasks, task]);
+    setTask("");
+  };
+
+  // Delete Task
+  const deleteTask = (index) => {
+    const newTasks = tasks.filter((_, i) => i !== index);
+    setTasks(newTasks);
+  };
+
+  return (
+    <div className="container">
+      <h1>To Do App ✅</h1>
+
+      <input
+        type="text"
+        placeholder="Enter task..."
+        value={task}
+        onChange={(e) => setTask(e.target.value)}
+      />
+
+      <button onClick={addTask}>Add</button>
+
+      <ul>
+        {tasks.map((t, index) => (
+          <li key={index}>
+            {t}
+            <button onClick={() => deleteTask(index)}>
+                Delete
+            </button>
+          </li>
+        ))}
+      </ul>
+    </div>
+  );
+}
+
+export default App;
