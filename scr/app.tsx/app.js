e41001444a3bc120118a06adec2cf7cef2c1e58d
jaferabdella674-eng <jaferabdella674@gmail.com>
2026-02-23 22:26:12 +0300

    update website

---
+++ scr/app.tsx/app.js
@@ -0,0 +1,9 @@
+function App() {
+  return (
+    <div>
+      <h1>Hello Jafer 👋</h1>
+      <p>This is my first React App</p>
+    </div>
+  )
+}
+
---
+++ scr/app.tsx/header.tsx
@@ -0,0 +1,7 @@
+function Header() {
+    return (
+      <h2>Welcome to Jafer Media 🚀</h2>
+    )
+  }
+  
+  export default Header
